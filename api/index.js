7ca4df555f94e4c4a5fafbb7efc5e64d2d151202
grayrey28-dev <grayrey28@gmail.com>
2026-04-20 08:27:50 -0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = (req, res) => {
   // Replace the URL below with the site you want to unblock
-  const target = "https://www.remoteplay.dl.playstation.net&deviceid=PHYS-SMIC-US-0000-3565&user=grreyes&rule=U3R1ZGVudCBEZWZhdWx0IFBvbGljeSAtIEJsb2Nr&ruleid=3f767f59-8bfc-4b13-a842-a3f03009ab4e&path=remoteplay/lang/en/index.html&method=rule_match&cid=Z3JyZXllczE3NTU4Nzg4MjYxNDY="; 
+  const target = "https://www.help.snapchat.com&deviceid=PHYS-SMIC-US-0000-3565&user=grreyes&rule=U3R1ZGVudCBEZWZhdWx0IFBvbGljeSAtIEJsb2Nr&ruleid=3f767f59-8bfc-4b13-a842-a3f03009ab4e&path=hc/en-us/articles/10341190387348-How-to-Change-Snapchat-for-Web-Permissions-for-&method=rule_match&cid=Z3JyZXllczE3NTU4Nzg4MjYxNDY=
 
   const proxy = createProxyMiddleware({
     target,
